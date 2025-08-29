3f5c5eec22adcf40f9aacfd681644b06b38eb375
Sebastian Gräßl <mail@bastilian.me>
2025-08-29 02:37:24 +0200

    feat(useFilterConfig): Add setFilter callback
    The `setFilter` callback allows setting a specific filter to a specific value.

--- src/hooks/useFilterConfig/useFilterConfig.stories.js
+++ src/hooks/useFilterConfig/useFilterConfig.stories.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Button, Card, CardBody, Content } from '@patternfly/react-core';
 
 import defaultStoryMeta from '~/support/defaultStoryMeta';
 import columns from '~/support/factories/columns';
-import {
+import filters, {
   customNumberFilter,
   customNumberFilterType,
   customGenresFilter,
@@ -16,10 +17,12 @@ import filtersSerialiser from '~/components/StaticTableToolsTable/helpers/serial
 import useExampleDataQuery from '~/support/hooks/useExampleDataQuery';
 
 import { TableToolsTable, TableStateProvider } from '~/components';
+import { useStateCallbacks } from '~/hooks';
 
 const queryClient = new QueryClient();
 
 const defaultOptions = {
+  debug: true,
   serialisers: {
     pagination: paginationSerialiser,
     sort: sortSerialiser,
@@ -84,6 +87,100 @@ export const CustomFiltersStory = {
     ),
   ],
   render: (args) => <CustomFiltersExample {...args} />,
+};
+
+const SetFilterExample = () => {
+  const {
+    current: { setFilter },
+  } = useStateCallbacks();
+  const {
+    loading,
+    result: { data, meta: { total } = {} } = {},
+    error,
+  } = useExampleDataQuery({
+    endpoint: '/api',
+    useTableState: true,
+  });
+
+  return (
+    <>
+      <Content>
+        <p>This story is to test the `setFilter`</p>
+      </Content>
+      <Card>
+        <CardBody>
+          <Button
+            onClick={() => {
+              setFilter('rating-above', [1]);
+            }}
+          >
+            1
+          </Button>
+          <Button
+            onClick={() => {
+              setFilter('rating-above', [2]);
+            }}
+          >
+            2
+          </Button>
+          <Button
+            onClick={() => {
+              setFilter('rating-above', [3]);
+            }}
+          >
+            3
+          </Button>
+          <Button
+            onClick={() => {
+              setFilter('rating-above', [4]);
+            }}
+          >
+            4
+          </Button>
+          <Button
+            onClick={() => {
+              setFilter('rating-above', [5]);
+            }}
+          >
+            5
+          </Button>
+          <Button
+            onClick={() => {
+              setFilter('title', ['ghost']);
+            }}
+          >
+            Set title filter to "ghost"
+          </Button>
+        </CardBody>
+      </Card>
+      <TableToolsTable
+        loading={loading}
+        items={data}
+        total={total}
+        error={error}
+        columns={columns}
+        filters={{
+          filterConfig: filters,
+        }}
+        options={{
+          ...defaultOptions,
+        }}
+      />
+    </>
+  );
+};
+
+export const SetFilterStory = {
+  decorators: [
+    (Story) => (
+      <QueryClientProvider client={queryClient}>
+        <TableStateProvider>
+          <Story />
+        </TableStateProvider>
+      </QueryClientProvider>
+    ),
+  ],
+  render: (args) => <SetFilterExample {...args} />,
 };
 
 export default meta;
