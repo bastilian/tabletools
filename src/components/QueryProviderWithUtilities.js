6b32ca4c8aaf80564545305cdf64e3290bec1a3f
Sebastian Gräßl <mail@bastilian.me>
2026-09-21 16:53:30 +0200

    refactor(Components): Move Query provider

--- src/components/QueryProviderWithUtilities.js
+++ src/components/QueryProviderWithUtilities.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import { QueryClientProvider } from '@tanstack/react-query';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { DEFAULT_QUERY_CLIENT } from './constants';
 
-const queryClient = new QueryClient();
-
-const QueryProviderWithUtilities = ({ children }) => (
-  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+const QueryProviderWithUtilities = ({
+  queryClient = DEFAULT_QUERY_CLIENT,
+  children,
+  ...props
+}) => (
+  <QueryClientProvider client={queryClient} {...props}>
+    {children}
+  </QueryClientProvider>
 );
 
 QueryProviderWithUtilities.propTypes = {
   children: propTypes.node,
+  queryClient: propTypes.object,
 };
 
 export default QueryProviderWithUtilities;
--- src/components/constants.js
+++ src/components/constants.js
@@ -1,7 +1,11 @@
 import DataViewTable from './DataViewTable';
+import { QueryClient } from '@tanstack/react-query';
+
 import DeprecatedTable from './DeprecatedTable';
 
 export const variants = {
   table: DeprecatedTable,
   dataViewTable: DataViewTable,
 };
+
+export const DEFAULT_QUERY_CLIENT = new QueryClient();
