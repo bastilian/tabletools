179d768111175c620378598fe53a5a98de01b0fe
Anastasiia Surai <68393563+Siasurai@users.noreply.github.com>
2026-09-22 11:52:35 +0200

    feat(useExport): add custom export filename (#143)

--- src/hooks/useExport/useExport.js
+++ src/hooks/useExport/useExport.js
@@ -14,15 +14,16 @@ import { downloadItems, exportableColumns } from './helpers';
 /**
  * Provides export props for table tools.
  *
- *  @param   {object}          [options]            AsyncTableTools options
- *  @param   {Function}        [options.exporter]   Function to return an array of items to be exported
- *  @param   {Array}           [options.columns]    columns for the export
- *  @param   {boolean}         [options.isDisabled] Wether or not export is enabled
- *  @param   {Function}        [options.onStart]    Function to call before the export
- *  @param   {Function}        [options.onComplete] Function to call when the export succeeded
- *  @param   {Function}        [options.onError]    Function to call when there was an error exporting
+ *  @param   {object}          [options]                AsyncTableTools options
+ *  @param   {Function}        [options.exporter]       Function to return an array of items to be exported
+ *  @param   {Array}           [options.columns]        columns for the export
+ *  @param   {boolean}         [options.isDisabled]     Wether or not export is enabled
+ *  @param   {string}          [options.exportFilename] Custom filename for the exported file
+ *  @param   {Function}        [options.onStart]        Function to call before the export
+ *  @param   {Function}        [options.onComplete]     Function to call when the export succeeded
+ *  @param   {Function}        [options.onError]        Function to call when there was an error exporting
  *
- *  @returns {useExportReturn}                      Export props, or `{}` when disabled
+ *  @returns {useExportReturn}                          Export props, or `{}` when disabled
  *
  *  @group Hooks
  *
@@ -31,6 +32,7 @@ const useExport = ({
   exporter,
   columns = [],
   isDisabled = false,
+  exportFilename,
   onStart,
   onComplete,
   onError,
@@ -46,7 +48,7 @@ const useExport = ({
       try {
         const items = await exporter(serialisedTableState, tableState);
 
-        downloadItems(exportColumns, items, format);
+        downloadItems(exportColumns, items, format, exportFilename);
         onComplete?.(items);
       } catch (error) {
         console.error(error);
@@ -59,6 +61,7 @@ const useExport = ({
       onComplete,
       exporter,
       exportColumns,
+      exportFilename,
       tableState,
       serialisedTableState,
     ],
